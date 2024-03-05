54278bf3d52a8cb5cdfb5ffec41a377b2e64709b
Tuhin Mallick <tuhin.mllk@gmail.com>
2024-03-05 11:34:44 +0000

    refactoring

--- server/lambda_function.py
+++ server/lambda_function.py
@@ -1,55 +1,55 @@
-import logging
+# import logging
 
-import clip_image_search.utils as utils
-from clip_image_search import CLIPFeatureExtractor, Searcher
-
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+# import clip_image_search.utils as utils
+# from clip_image_search import CLIPFeatureExtractor, Searcher
 
 
-feature_extractor = CLIPFeatureExtractor()
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
 
 
-def lambda_handler(event, context):
-    for key in ("input_type", "query"):
-        if key not in event:
-            logger.error(f"Key '{key}' not found in event.")
-            return {
-                "status_code": 400,
-                "message": "Missing input.",
-            }
+# feature_extractor = CLIPFeatureExtractor()
 
-    try:
-        input_type = event["input_type"]
-        if input_type == "text":
-            text = event["query"]
-            query_features = feature_extractor.get_text_features(text)
-        elif input_type == "image":
-            image_url = event["query"]
-            image = utils.load_image_from_url(image_url)
-            query_features = feature_extractor.get_image_features(image)
-        else:
-            raise ValueError(f"Unrecognized input type: {input_type}")
-    except Exception as e:
-        logger.error(e)
-        return {
-            "status_code": 400,
-            "message": "Error when processing input.",
-        }
 
-    try:
-        searcher = Searcher()
-        response = searcher.knn_search(query_features[0], k=10)
-        results = response["hits"]["hits"]
-        return {
-            "status_code": 200,
-            "message": "Success",
-            "body": results,
-        }
-    except Exception as e:
-        logger.error(e)
-        return {
-            "status_code": 500,
-            "message": "Search engine error.",
-        }
+# def lambda_handler(event, context):
+#     for key in ("input_type", "query"):
+#         if key not in event:
+#             logger.error(f"Key '{key}' not found in event.")
+#             return {
+#                 "status_code": 400,
+#                 "message": "Missing input.",
+#             }
+
+#     try:
+#         input_type = event["input_type"]
+#         if input_type == "text":
+#             text = event["query"]
+#             query_features = feature_extractor.get_text_features(text)
+#         elif input_type == "image":
+#             image_url = event["query"]
+#             image = utils.load_image_from_url(image_url)
+#             query_features = feature_extractor.get_image_features(image)
+#         else:
+#             raise ValueError(f"Unrecognized input type: {input_type}")
+#     except Exception as e:
+#         logger.error(e)
+#         return {
+#             "status_code": 400,
+#             "message": "Error when processing input.",
+#         }
+
+#     try:
+#         searcher = Searcher()
+#         response = searcher.knn_search(query_features[0], k=10)
+#         results = response["hits"]["hits"]
+#         return {
+#             "status_code": 200,
+#             "message": "Success",
+#             "body": results,
+#         }
+#     except Exception as e:
+#         logger.error(e)
+#         return {
+#             "status_code": 500,
+#             "message": "Search engine error.",
+#         }
--- server/test_lambda_function.py
+++ server/test_lambda_function.py
@@ -1,31 +1,31 @@
-import pytest
+# import pytest
 
-from lambda_function import lambda_handler
-
-
-@pytest.mark.parametrize(
-    "event",
-    [
-        {"input_type": "text", "query": "two cats"},
-        {"input_type": "image", "query": "https://i.imgur.com/jlFgGpe.jpeg"},
-    ],
-)
-def test_text_input(event):
-    response = lambda_handler(event, None)
-    assert response["status_code"] == 200
-    assert len(response["body"]) == 10
+# from lambda_function import lambda_handler
 
 
-@pytest.mark.parametrize(
-    "event",
-    [
-        {},
-        {"input_type": "text"},
-        {"query": "two cats"},
-        {"input_type": "language", "query": "two cats"},
-        {"input_type": "image", "query": "this_url_does_not_exist"},
-    ],
-)
-def test_invalid_inputs(event):
-    response = lambda_handler(event, None)
-    assert response["status_code"] == 400
+# @pytest.mark.parametrize(
+#     "event",
+#     [
+#         {"input_type": "text", "query": "two cats"},
+#         {"input_type": "image", "query": "https://i.imgur.com/jlFgGpe.jpeg"},
+#     ],
+# )
+# def test_text_input(event):
+#     response = lambda_handler(event, None)
+#     assert response["status_code"] == 200
+#     assert len(response["body"]) == 10
+
+
+# @pytest.mark.parametrize(
+#     "event",
+#     [
+#         {},
+#         {"input_type": "text"},
+#         {"query": "two cats"},
+#         {"input_type": "language", "query": "two cats"},
+#         {"input_type": "image", "query": "this_url_does_not_exist"},
+#     ],
+# )
+# def test_invalid_inputs(event):
+#     response = lambda_handler(event, None)
+#     assert response["status_code"] == 400
